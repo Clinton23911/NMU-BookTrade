4b6effa266b2e82ba5ec7c9e56d9a05696285c1c
pc <pc@DESKTOP-5JOMBPT>
2025-05-19 19:13:24 +0200

    Add project files.

---
+++ Register.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="NMU_BookTrade.WebForm6" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="middle_section" runat="server">
+
+
+</asp:Content>
---
+++ Site1.Master.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NMU_BookTrade
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            
+            if (!IsPostBack)
+            {
+                // Default: Anonymous is visible, others are hidden
+                MenuAnonymous.Visible = true;
+                MenuAdmin.Visible = false;
+                MenuBuyer.Visible = false;
+                MenuSeller.Visible = false;
+                MenuDriver.Visible = false;
+
+                if (Session["AccessID"] != null)
+                {
+                    MenuAnonymous.Visible = false; // Hide Anonymous if user is logged in
+
+                    switch (Session["AccessID"].ToString())
+                    {
+                        case "1":
+                            MenuAdmin.Visible = true;
+                            break;
+                        case "2":
+                            MenuBuyer.Visible = true;
+                            break;
+                        case "3":
+                            MenuSeller.Visible = true;
+                            break;
+                        case "4":
+                            MenuDriver.Visible = true;
+                            break;
+                        default:
+                            MenuAnonymous.Visible = true; // Fallback to Anonymous if AccessID is unexpected
+                            break;
+                    }
+                }
+            }
+        }
+
+
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Register.aspx");
+        }
+
+        protected void btnLogIn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Log In.aspx");
+        }
+
+
+    }
+}

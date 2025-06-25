f0ad80e01601251767186c32aa813e43902d47eb
NkanyaneP <paballonkanyane@gmail.com>
2025-06-25 20:30:22 +0200

    Logout Update

--- User Management/LogOut.aspx.cs
+++ User Management/LogOut.aspx.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,9 +14,51 @@ namespace NMU_BookTrade
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Session.Clear();
-            Response.Redirect("~/User Management/Home.aspx");
+            if (!IsPostBack && Request.UrlReferrer != null)
+            {
+                Session["PrevPage"] = Request.UrlReferrer.ToString();
+            }
         }
 
+        private string GetCustomerName(string userId)
+        {
+            string name = "User";
+            string connectionString = ConfigurationManager.ConnectionStrings["NMUBookTradeConnection"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT FullName FROM Buyer WHERE BuyerID = buyerID";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@UserId", userId);
+
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    name = reader["FullName"].ToString();
+                }
+                con.Close();
+            }
+
+            return name;
+        }
+
+        protected void btnYes_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("Login.aspx");
+        }
+
+        protected void btnNo_Click(object sender, EventArgs e)
+        {
+            if (Session["PreviousPage"] != null)
+            {
+                Response.Redirect(Session["PreviousPage"].ToString());
+            }
+            else
+            {
+                Response.Redirect("Home.aspx");
+            }
+        }
     }
 }
